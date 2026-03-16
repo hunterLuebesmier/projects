075a2730e393d9166ddbc57d66b4e4b65e645d95
hunterLuebesmier <hunterluebesmier69@gmail.com>
2026-03-16 16:45:00 +0000

    print year

---
+++ The_year.java
@@ -0,0 +1,8 @@
+public class The_year {
+public static void main(String[] args) {
+    int year = 2026;
+System.out.println("welcome to java!");
+System.out.println("The Year is, " + year);
+}
+    
+}

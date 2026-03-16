848b65ca4100b4d3dedd9e37edc5d4906e995293
hunterLuebesmier <hunterluebesmier69@gmail.com>
2026-03-16 16:26:45 +0000

    print function

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld {
+public static void main(String[] args) {
+    System.out.println("hunter");
+    System.out.println("my favorite subject is coding and computers");
+    System.out.println("the year is 2026 ");
+    System.out.println("i want to learn more about java that i dont already know");
+}
+    
+}
